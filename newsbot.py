4b1000044d2d8df0c803df0bd3c87375503a247f
Nidhish Reddy <nidhishreddy986@gmail.com>
2026-02-07 00:12:51 +0530

    Fixed environment variables

--- newsbot.py
+++ newsbot.py
@@ -1,10 +1,11 @@
 import feedparser
-from telegram import Bot
 import os
+import time
+from telegram import Bot
 
 RSS_URL = "https://www.automotiveworld.com/feed/"
-BOT_TOKEN = os.environ.get("8226886589:AAExiXPah_A6jRWBluSjB-KhIT6ZEbrBUhM")
-CHAT_ID = os.environ.get("6465154955")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+CHAT_ID = os.environ.get("CHAT_ID")
 
 bot = Bot(token=BOT_TOKEN)
 
@@ -13,15 +14,26 @@ SENT_FILE = "sent_articles.txt"
 if not os.path.exists(SENT_FILE):
     open(SENT_FILE, "w").close()
 
-with open(SENT_FILE, "r") as f:
-    sent_links = f.read().splitlines()
+print("Bot started...")
 
-feed = feedparser.parse(RSS_URL)
+while True:
+    try:
+        with open(SENT_FILE, "r") as f:
+            sent_links = f.read().splitlines()
 
-for entry in feed.entries:
-    if entry.link not in sent_links:
-        message = f"📰 {entry.title}\n\n{entry.summary}\n\nRead more: {entry.link}"
-        bot.send_message(chat_id=CHAT_ID, text=message)
+        feed = feedparser.parse(RSS_URL)
 
-        with open(SENT_FILE, "a") as f:
-            f.write(entry.link + "\n")
+        for entry in feed.entries:
+            if entry.link not in sent_links:
+                message = f"📰 {entry.title}\n\n{entry.summary}\n\nRead more: {entry.link}"
+                bot.send_message(chat_id=CHAT_ID, text=message)
+
+                with open(SENT_FILE, "a") as f:
+                    f.write(entry.link + "\n")
+
+        print("Checked RSS. Sleeping 10 minutes...")
+        time.sleep(600)
+
+    except Exception as e:
+        print("Error:", e)
+        time.sleep(60)
